77592b78adb1e3857290dc0903c68e935c92871e
salskhal <olaniyisal@gmail.com>
2023-11-28 23:15:55 +0100

    Feat/ completed work with framer motion animation included

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -4,29 +4,28 @@ import { fetchAnime } from "@/app/action";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
-import AnimeCard, { AnimeProp } from "./AnimeCard";
+import AnimeCard from "./AnimeCard";
 
-let page = 2
+let page = 2;
 
+export type AnimeCard = JSX.Element
 
 function LoadMore() {
   const { ref, inView } = useInView();
-  const [data, setData] = useState<AnimeProp[]>([]);
+  const [data, setData] = useState<AnimeCard[]>([]);
 
   useEffect(() => {
     if (inView) {
       fetchAnime(page).then((res) => {
         setData([...data, ...res]);
-        page++
+        page++;
       });
     }
   }, [inView, data]);
   return (
     <>
-    <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
